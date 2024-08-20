3429f49683db6c3db5b13e8798333f4eb446d049
Amiran <kotiaamiran@gmail.com>
2024-08-20 22:39:33 +0400

    postrgresql added

--- store/settings.py
+++ store/settings.py
@@ -68,8 +68,12 @@ WSGI_APPLICATION = 'store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'qXCkIUKJWeEVbyILdRTkqqudcTohxqSy',
+        'HOST': 'junction.proxy.rlwy.net',
+        'PORT': '47384',
     }
 }
 
